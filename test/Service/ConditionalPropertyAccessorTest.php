c09ce24e648d88282448c72511b0361e5aee6320
Emily Shepherd <emily@emilyshepherd.me>
2017-02-12 17:08:44 +0000

    Correct spelling in file header

--- test/Service/ConditionalPropertyAccessorTest.php
+++ test/Service/ConditionalPropertyAccessorTest.php
@@ -4,7 +4,7 @@
  *
  * (c) Emily Shepherd <emily@emilyshepherd.me>
  *
- * For the full copyright and licence information, please view the
+ * For the full copyright and license information, please view the
  * LICENSE.md file that was distributed with this source code.
  *
  * @package spaark/composite-utils
