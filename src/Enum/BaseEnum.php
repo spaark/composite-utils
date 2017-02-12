1f9a785165c5b22c4b00f44ffe2ec7d65cc5b538
Emily Shepherd <emily@emilyshepherd.me>
2017-02-12 16:54:38 +0000

    Add header to all files

--- src/Enum/BaseEnum.php
+++ src/Enum/BaseEnum.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of the Composite Utils package.
+ *
+ * (c) Emily Shepherd <emily@emilyshepherd.me>
+ *
+ * For the full copyright and licence information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package spaark/composite-utils
+ * @author Emily Shepherd <emily@emilyshepherd>
+ * @license MIT
+ */
 
 namespace Spaark\CompositeUtils\Enum\BaseEnum;
 
--- src/Model/Reflection/ReflectionProperty.php
+++ src/Model/Reflection/ReflectionProperty.php
@@ -1,4 +1,18 @@
-<?php namespace Spaark\CompositeUtils\Model\Reflection;
+<?php
+/**
+ * This file is part of the Composite Utils package.
+ *
+ * (c) Emily Shepherd <emily@emilyshepherd.me>
+ *
+ * For the full copyright and licence information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package spaark/composite-utils
+ * @author Emily Shepherd <emily@emilyshepherd>
+ * @license MIT
+ */
+
+namespace Spaark\CompositeUtils\Model\Reflection;
 /**
  *
  *
