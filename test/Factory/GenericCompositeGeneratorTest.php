73677473ea61cd15c05120be04be8605ff287d48
Emily Shepherd <emily@emilyshepherd.me>
2017-03-14 22:40:33 +0000

    Improve GenericCompositeGeneratorTest

--- test/Factory/GenericCompositeGeneratorTest.php
+++ test/Factory/GenericCompositeGeneratorTest.php
@@ -16,10 +16,13 @@ namespace Spaark\CompositeUtils\Test\Factory;
 
 use Spaark\CompositeUtils\Factory\Reflection\ReflectionCompositeFactory;
 use Spaark\CompositeUtils\Factory\Reflection\GenericCompositeGenerator;
+use Spaark\CompositeUtils\Service\GenericNameProvider;
 use Spaark\CompositeUtils\Test\Model\TestGenericEntity;
+use Spaark\CompositeUtils\Test\Model\TestEntity;
 use Spaark\CompositeUtils\Model\Reflection\Type\StringType;
 use Spaark\CompositeUtils\Model\Reflection\Type\ObjectType;
 use Spaark\CompositeUtils\Model\Collection\ArrayList;
+use Spaark\CompositeUtils\Model\Generic\GenericContext;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -27,25 +30,101 @@ use PHPUnit\Framework\TestCase;
  */
 class GenericCompositeGeneratorTest extends TestCase
 {
-    public function testA()
+    /**
+     * @var GenericCompositeProvider
+     */
+    private $genericCompositeGenerator;
+
+    /**
+     * @var ReflectionComposite
+     */
+    private $composite;
+
+    /**
+     * @var GenericNameProvider
+     */
+    private $genericNameProvider;
+
+    /**
+     * @var ObjectType
+     */
+    private $object;
+
+    public function setUp() : void
     {
-        $reflect = ReflectionCompositeFactory::fromClassName
+        $this->composite = ReflectionCompositeFactory::fromClassName
         (
             TestGenericEntity::class
         )->build();
 
-        $generic = new GenericCompositeGenerator($reflect);
-        $code = $generic->generateClassCode
+        $this->genericCompositeGenerator
+            = new GenericCompositeGenerator($this->composite);
+
+        $this->object = new ObjectType(TestGenericEntity::class);
+        $this->object->generics->add(new StringType());
+        $this->object->generics->add(new ObjectType(TestEntity::class));
+        $this->genericNameProvider = new GenericNameProvider
         (
-            new StringType(),
-            new ObjectType('FullClassName', '')
+            new GenericContext($this->object, $this->composite)
+        );
+    }
+
+    protected function generateCode()
+    {
+        return $this->genericCompositeGenerator->generateClassCode
+        (
+            $this->object->generics[0],
+            $this->object->generics[1]
+        );
+    }
+
+    public function testGeneratedClassCodeName()
+    {
+        $code = $this->generateCode();
+        $classname =
+            $this->genericNameProvider->inferName($this->object);
+
+        $this->assertContains
+        (
+            'class ' . $classname->classname,
+            $code
         );
 
         $this->assertContains
         (
-            ArrayList::class . '_gFullClassName_e $a',
+            'namespace ' . $classname->namespace,
+            $code
+        );
+    }
+
+    public function testGeneratedClassCodeArguments()
+    {
+        $code = $this->generateCode();
+
+        $argumentA = new ObjectType(ArrayList::class);
+        $argumentA->generics->add(new ObjectType(TestEntity::class));
+
+        $this->assertContains
+        (
+            (string)$this->genericNameProvider->inferName($argumentA),
             $code
         );
         $this->assertContains('string $b', $code);
+
+        return $code;
+    }
+
+    public function testCreateClass()
+    {
+        $this->genericCompositeGenerator->createClass
+        (
+            $this->object->generics[0],
+            $this->object->generics[1]
+        );
+
+        $this->assertTrue(class_exists
+        (
+            $this->genericNameProvider->inferName($this->object)
+        ));
     }
 }
